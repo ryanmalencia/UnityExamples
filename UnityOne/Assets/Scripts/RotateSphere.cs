1433b375a34afdee455996fd30a33498b070d40a
Ryan Malencia <ryan.malencia@pitt.edu>
2018-08-18 16:55:45 -0400

    Scripts

---
+++ UnityOne/Assets/Scripts/RotateSphere.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSphere : MonoBehaviour {
+	Vector3 movement;
+	// Use this for initialization
+	void Start () {
+		movement = new Vector3 (100, 75, 50);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (movement * Time.deltaTime);
+	}
+}
